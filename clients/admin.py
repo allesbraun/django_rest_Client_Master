9a7d35141e4a405af548f3f312400c7f51ae081a
Gabriel Braun Rodenbusch <allesbraun123@gmail.com>
2023-07-12 19:51:51 -0300

    ordenation and filtering of clients

--- clients/admin.py
+++ clients/admin.py
@@ -6,10 +6,11 @@ from clients.models import Client
 class Clients(admin.ModelAdmin):
     list_display = ('id', 'name', 'email','cpf', 'rg', 'cellphone', 'active')
     list_display_links = ('id', 'name')
-    search_fields = ('name',)
-    list_filter = ('active',)
-    list_editable = ('active',)
-    list_per_page = 25
+    search_fields = ('name', )
+    list_filter = ('active', )
+    list_editable = ('active', )
+    list_per_page = 10
+    ordering = ('name', )
 
 admin.site.register(Client, Clients)
 
